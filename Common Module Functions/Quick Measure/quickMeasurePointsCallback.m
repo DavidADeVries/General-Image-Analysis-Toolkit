8d9a289f1324eb6a423244c86a82fdfecd420d3f
DavidADeVries <ddevries@uwaterloo.ca>
2015-07-13 09:06:33 -0400

    Restructuring of the directory structure
    - now have the main code and then a directory for functions or features
    that only some submodules would share

---
+++ Common Module Functions/Quick Measure/quickMeasurePointsCallback.m
@@ -0,0 +1,32 @@
+function [ ] = quickMeasurePointsCallback( hObject)
+%saveQuickMeasurePoints as quick measure line is dragged around in real-time, their
+%position is pushed back up to the quickMeasurePoint field. Label is also
+%updated
+
+handles = guidata(hObject);
+
+handles.updateUndoCache = true; %make sure the undo updater is waiting on click up
+
+currentFile = getCurrentFile(handles);
+
+quickMeasurePoints = getPosition(handles.quickMeasureLineHandle);
+
+currentFile = currentFile.setQuickMeasurePoints(quickMeasurePoints);
+
+% updateUndo/pendingChanges should only be done at end of click and drag
+% (clickup listener callback)
+updateUndo = false;
+changesPending = false;
+
+handles = updateFile(currentFile, updateUndo, changesPending, handles);
+
+%update line/text
+toggled = false;
+
+handles = drawQuickMeasureWithCallback(currentFile, handles, hObject, toggled);
+
+%push up changes
+guidata(hObject, handles);
+
+end
+
